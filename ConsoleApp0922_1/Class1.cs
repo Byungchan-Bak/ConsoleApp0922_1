7dd67286a21f8f74a6cedf20107c180f8ca6643c
병찬 <oceans0221@nate.com>
2020-09-22 10:59:22 +0900

    초기 커밋.

---
+++ ConsoleApp0922_1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0922_1
+{
+    class Class1
+    {
+        static int Sum(int a)
+        {
+            int i = 0;
+            int sum = 0;
+
+            while(i <= a)
+            {
+                sum += i;
+                i++;
+            }
+
+            return sum;
+        }
+
+        static int Sum(int step, int min = 1, int max = 100)    //필수 파라미터는 선택 파라미터 앞에 있어야 됨.
+        {
+            int i = min;
+            int tot = 0;
+            while(i <= max)
+            {
+                tot += i;
+                i += step;
+            }
+
+            return tot;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(Sum(1, max : 10, min : 1));  //명명 파라미터 -->> 전달되는 매개변수를 정확이 명시해줌.
+            Console.WriteLine(Sum(1, 100)); //step, min
+            Console.WriteLine(Sum(1));  //step
+        }
+    }
+}
---
+++ ConsoleApp0922_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0922_1
+{
+    class Program
+    {
+        static void Sqr(int x)  //제곱 메서드 -->> return해야 Main에서 사용가능
+        {
+            x = x * x;
+        }
+        static int Sqr(ref int x)  //ref함수를 이용하여 파라미터 값 수신 -->> 파라미터 값(값)이 존재해야 사용가능
+        {
+            return x * x;
+        }
+        static void GetName(out string name, out int id)    //out함수를 이용하여 파라미터 값 수신 -->> 파라미터 값(주소)이 존재하지 않아도 되나 메서드에서 정의 해야함.
+        {
+            Console.WriteLine("Enter Name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter ID : ");
+            id = int.Parse(Console.ReadLine());
+        }
+
+        static void PrintIntParams(params int[] arr)   //Params 가변배열
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int a = 3;
+            Sqr(a);
+            Console.WriteLine("Value -->> {0}", a); //3
+
+            int b = 3;
+            Sqr(ref b);
+            Console.WriteLine("ref Value -->> {0}", b); //9
+
+            string name;
+            int id;
+            GetName(out name, out id);
+            Console.WriteLine("out -->> {0}\t {1}", name, id);  //name, id
+
+            PrintIntParams(1, 2, 3, 4);
+            int[] myArr = { 5, 6, 7, 8, 9 };
+            PrintIntParams(myArr);  //매개변수가 배열이어도 가능
+            PrintIntParams();   //매개변수가 없어도 가능
+        }
+    }
+}
